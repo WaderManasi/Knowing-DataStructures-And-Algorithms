ed8f95a2b5688c595fce095ae185e160b1bb882f
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-04 17:40:12 +0530

    Prime no opt

---
+++ Prime Numbers/PrimeNumber.cpp
@@ -0,0 +1,16 @@
+public:
+    int isPrime(int no){
+        if (no == 2)
+            return 1;
+        if (no % 2 == 0 || no == 1)
+            return 0;
+
+        for (int i = 2; i <= sqrt(no); i++)
+        {
+            if (no % i == 0)
+               return 0;
+        }
+        return 1;
+    
+    }
+};
