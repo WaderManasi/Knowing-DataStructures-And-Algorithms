31f58559455d70f08c58f5aae9852dc1e0fb2e36
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-04 15:06:48 +0530

    Sum of prime factor of number

---
+++ Prime Numbers/SumOfPrimes.cpp
@@ -0,0 +1,34 @@
+class Solution{
+public:
+    int isPrime(int no)
+    {
+        if(no==2)
+            return 1;
+            
+        if(no%2==0 || no==1)
+            return 0;
+        
+        int flag=0;
+        for(int i=2;i<no;i++)
+        {
+            //cout<<no%i<<"mod  ";
+            if(no%i == 0)
+            flag=1;
+        }
+        //cout<<flag<<"f \n";
+        return flag==1?0:1;
+    }
+    int primeSum(int N){
+        int sum=0;
+        while(N>0)
+        {
+            int p=N%10;
+            if(isPrime(p))
+            {
+             sum+=p;
+            }
+            N=N/10;
+        }
+        return sum;
+    }
+};
