89e902544c85efd929a3b1c0c838f0a45fb675f1
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-05 23:41:28 +0530

    Prime factorS

---
+++ Prime Numbers/PrimeFactors.cpp
@@ -0,0 +1,30 @@
+class Solution{
+	public:
+	bool isPrime(int n)
+	{
+	    if(n==2)    return true;
+	    if(n==1 || n%2==0)  return false;
+	    for(int i=2;i<n;i++)
+	    {
+	        if(n%i == 0)    return false;
+	    }
+	    return true;
+	}
+	vector<int> AllPrimeFactors(int N) {
+	    vector<int>v;
+	    if(isPrime(N))
+	    {
+	        v.emplace_back(N);
+	        return v;
+	    }
+	    for(int i=2;i<N;i++)
+	    {
+	        if(N%i == 0)
+	        {
+	            if(isPrime(i))
+	            v.emplace_back(i);
+	        }
+	    }
+	    return v;
+	}
+};
