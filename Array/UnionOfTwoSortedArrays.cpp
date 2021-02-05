e5b3b2c6d57b72f3bf04ee34dff7437062a9f6d5
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-05 12:01:31 +0530

    Union of two sorted arrays

---
+++ Array/UnionOfTwoSortedArrays.cpp
@@ -0,0 +1,17 @@
+vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+{
+    vector<int>v;
+    vector<int>::iterator ip;
+    for(int i=0;i<n;i++)
+    {
+        v.emplace_back(arr1[i]);
+    }
+    for(int i=0;i<m;i++)
+    {
+        v.emplace_back(arr2[i]);
+    }
+    sort(v.begin(),v.end());
+    ip = unique(v.begin(), v.begin()+v.size());
+    v.resize(distance(v.begin(), ip)); 
+    return v;
+}
