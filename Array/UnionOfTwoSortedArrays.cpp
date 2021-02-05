ef6a759f11a8b476ece84749595c411958a5ea57
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-05 12:02:17 +0530

    Union of two sorted arrays

--- Array/UnionOfTwoSortedArrays.cpp
+++ Array/UnionOfTwoSortedArrays.cpp
@@ -1,3 +1,8 @@
+
+// Union of two arrays can be defined as the common and distinct elements in the two arrays.
+// Given two sorted arrays of size N and M respectively, find their union.
+
+
 vector<int> findUnion(int arr1[], int arr2[], int n, int m)
 {
     vector<int>v;
