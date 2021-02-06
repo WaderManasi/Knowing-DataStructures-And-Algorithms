0a58468cb668ba52623492ef007ae38ed183f412
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-07 00:14:19 +0530

    First Repeating element in array

--- Array/FirstRepeatingElement.cpp
+++ Array/FirstRepeatingElement.cpp
@@ -1,6 +1,5 @@
-Given an array arr[] of size N, find the first repeating element. The element should occurs more than once and the index of its first occurrence should be the smallest.
+// Given an array arr[] of size N, find the first repeating element. The element should occurs more than once and the index of its first occurrence should be the smallest.
 
- 
 //Solution:
 int firstRepeated(int arr[], int n) {
    for(int i=0;i<n;i++)
