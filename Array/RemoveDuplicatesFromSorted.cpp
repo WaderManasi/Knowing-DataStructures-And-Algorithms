5f4c450eb1b1c427b2826a953a858e0886f458bd
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-06 12:44:42 +0530

    Remove duplicates from sorted array

---
+++ Array/RemoveDuplicatesFromSorted.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        if(n==1)    return 1;
+        int i=0;
+        for(int j=1;j<n;j++)
+        {
+            if(a[i]!=a[j])
+            {
+                i++;
+                a[i]=a[j];
+            }
+        }
+        return i+1;
+    }
+};
