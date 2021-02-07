dcb6d3d8a94565a54b83e72518b6909483a5c6c6
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-07 23:19:38 +0530

    Printing non-repeating elements in effficient time

---
+++ Tricky_Problems/PrintNonRepeated.cpp
@@ -0,0 +1,18 @@
+// Function to print the non repeated elements in the array
+// arr[]: input array
+// n: size of array
+vector<int> printNonRepeated(int arr[],int n)
+{
+    vector<int>v;
+    unordered_map<int,int>m;
+    for(int i=0;i<n;i++)
+    {
+        m[arr[i]]++;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(m[arr[i]]==1)
+        v.emplace_back(arr[i]);
+    }
+    return v;
+}
