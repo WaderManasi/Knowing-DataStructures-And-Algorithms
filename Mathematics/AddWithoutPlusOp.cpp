13a0fb6e960835464ad6f54d4307dec7143ffedd
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-08 12:28:49 +0530

    Add without using + operator

---
+++ Mathematics/AddWithoutPlusOp.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    int sum(int x , int y)
+    {
+        // if(a>b)
+        // {
+        //     while(b--)
+        //     a++;
+        //     return a;
+        // }
+        // else
+        // {
+        //     while(a--)
+        //     b++;
+        //     return b;   
+        // }
+        while (y != 0)  
+    {  
+        // carry now contains common  
+        //set bits of x and y  
+        int carry = x & y;  
+  
+        // Sum of bits of x and y where at  
+        //least one of the bits is not set  
+        x = x ^ y;  
+  
+        // Carry is shifted by one so that adding  
+        // it to x gives the required sum  
+        y = carry << 1;  
+    }  
+    return x;  
+        
+    }
+};
