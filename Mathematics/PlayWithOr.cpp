f42422a361424752acc0d168f403c4e3a690a928
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-08 12:20:50 +0530

    OR operation

--- Mathematics/PlayWithOr.cpp
+++ Mathematics/PlayWithOr.cpp
@@ -1,5 +1,5 @@
-You are given an array arr[], you have to re-construct an array arr[].
-The values in arr[] are obtained by doing OR(bitwise or) of consecutive elements in the array.
+// You are given an array arr[], you have to re-construct an array arr[].
+// The values in arr[] are obtained by doing OR(bitwise or) of consecutive elements in the array.
 
 int* game_with_number(int arr[], int n)
 {
