5f7a1a3a0060da49ca01554002ad1fee8f020094
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-15 11:24:18 +0530

    Strong nos

--- Mathematics/StrongNumber.cpp
+++ Mathematics/StrongNumber.cpp
@@ -1,3 +1,6 @@
+Strong Numbers are the numbers whose sum of factorial of digits is equal to the original number. Given a number, 
+the task is to check if it is a Strong Number or not.
+
 class Solution
 {
 public:
