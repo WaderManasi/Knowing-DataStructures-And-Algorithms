fca52aa7ec9a34f4d09aa60b520dfa44d184b6a9
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-16 09:58:48 +0530

    Max area

--- Mathematics/MaximumArea.cpp
+++ Mathematics/MaximumArea.cpp
@@ -1,5 +1,5 @@
-Given the maximum possible area of the right angle triangle for a fixed length of hypotenuse. The task is to find the length of hypotenuse.
-Note: If the answer comes in Decimal, find it's Floor value.
+// Given the maximum possible area of the right angle triangle for a fixed length of hypotenuse. The task is to find the length of hypotenuse.
+// Note: If the answer comes in Decimal, find it's Floor value.
 
 class Solution {
   public:
