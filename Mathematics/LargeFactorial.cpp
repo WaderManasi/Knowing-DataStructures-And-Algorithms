94ae9b14a374a0104ad5601636ed903c5a1c83d8
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-17 09:52:31 +0530

    Large datatype in cpp

---
+++ Mathematics/LargeFactorial.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include<boost/multiprecision/cpp_int.hpp>
+using namespace std;
+namespace mp=boost::multiprecision;
+mp::cpp_int fact(int n)
+{
+    if(n<=1)    return 1;
+    return n*fact(n-1);
+}
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    cout<<fact(n)<<"\n";
+	}
+	return 0;
+}
