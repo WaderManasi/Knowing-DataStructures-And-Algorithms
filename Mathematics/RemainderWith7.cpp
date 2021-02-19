5d44249fe829dc36fb9aa3793183f8d0200e8c7d
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-19 15:25:53 +0530

    Mod 7

--- Mathematics/RemainderWith7.cpp
+++ Mathematics/RemainderWith7.cpp
@@ -1,7 +1,7 @@
-Given a number as string(n) , 
-find the remainder of the number whe it is divided by 7
+// Given a number as string(n) , 
+// find the remainder of the number whe it is divided by 7
 
-/*You are required to complete this method */
+//
 int remainderWith7(string n)
 {
     int x=0;
