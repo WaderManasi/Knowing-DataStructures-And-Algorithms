82d88145efe0ee539574cc4d122d60df57eaf86c
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-21 22:05:22 +0530

    Leetcode: move to zeros

---
+++ Tricky_Problems/MoveZeros.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int>v;
+        int ct=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)  ct++;
+            else
+                v.push_back(nums[i]);
+        }
+        while(ct--)
+            v.push_back(0);
+        for(int i=0;i<v.size();i++)
+            nums[i]=v[i];
+    }
+};
