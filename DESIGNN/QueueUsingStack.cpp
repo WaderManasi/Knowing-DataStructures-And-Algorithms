1e56a5535cb898c83d1bac4571c003a4b3f508fc
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-22 15:24:56 +0530

    Queue Using 2 Stacks

--- DESIGNN/QueueUsingStack.cpp
+++ DESIGNN/QueueUsingStack.cpp
@@ -3,7 +3,6 @@
 // (i) 1 x (a query of this type means  pushing 'x' into the queue)
 // (ii) 2   (a query of this type means to pop element from queue and print the poped element)
 
-
 /* The method push to push element into the queue */
 void StackQueue :: push(int x)
  {
