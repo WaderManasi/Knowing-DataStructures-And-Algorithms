2ebf7f2f640bfffa7f68bc46f2a8b02be0884f85
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-22 16:10:28 +0530

    Converting string to integer

--- String/implement_atoi.cpp
+++ String/implement_atoi.cpp
@@ -1,5 +1,5 @@
-Your task  is to implement the function atoi.
-The function takes a string(str) as argument and converts it to an integer and returns it.
+// Your task  is to implement the function atoi.
+// The function takes a string(str) as argument and converts it to an integer and returns it.
 
 int atoi(string str)
 {
