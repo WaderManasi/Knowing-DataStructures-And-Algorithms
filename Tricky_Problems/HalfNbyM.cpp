e056b148eaf6480ad138a59b72c49d4f37577926
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-24 22:50:45 +0530

    Halving nos: efficient solution

--- Tricky_Problems/HalfNbyM.cpp
+++ Tricky_Problems/HalfNbyM.cpp
@@ -1,6 +1,19 @@
 // Given two values N and M. 
 // Give the value when N is halved M-1 times.
 
+//TLE
+// class Solution{
+// public:
+//     int mthHalf(int N, int M){
+//         int m=M-1;
+//         while(m>0)
+//         {
+//             N=N/2;
+//         }
+//         return N;
+//     }
+// };
+
 //Optimize solution to find halving of no
 class Solution{
 public:
