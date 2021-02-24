9febc74fc7004c4130e4ed9272f9d0347aa278fb
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-24 23:39:52 +0530

    Amicable pair

--- Mathematics/AmicablePair.cpp
+++ Mathematics/AmicablePair.cpp
@@ -1,3 +1,8 @@
+"""
+Amicable numbers are two different numbers so related that the sum of the proper divisors of each is equal to the other number. (A proper divisor of a number is a positive factor of that number other than the number itself. Given two Numbers A and B, find whether they are Amicable Numbers or not.
+Print 1 if they are Amicable else 0.
+"""
+
 class Solution:
     def isAmicable(self, A , B):
         if(A==B):
