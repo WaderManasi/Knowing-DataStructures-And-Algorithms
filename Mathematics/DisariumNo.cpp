bd8bfc022dc9172bc3c927d703d92a9ede975e4e
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-25 12:00:46 +0530

    Disarium no

--- Mathematics/DisariumNo.cpp
+++ Mathematics/DisariumNo.cpp
@@ -1,3 +1,7 @@
+// Given a number N, find if it is Disarium or not. A number is called Disarium if sum of its digits powered with their respective positions is equal to the number itself.
+// Output 1 if it's Disarium, and 0 if not.
+
+
 int isDisarium(int N) {
         int i=floor(log10(N)+1);
         int n=N;
