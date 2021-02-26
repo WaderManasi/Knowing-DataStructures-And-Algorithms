e43d9d25d8598c81d376c4b7b34e9e6f06234905
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-02-26 10:49:52 +0530

    Median of 2 sorted arrays

---
+++ Tricky_Problems/MedianOf2SortedArrays.cpp
@@ -0,0 +1,13 @@
+// arr : given array with size n 
+// brr : given array with size m
+int findMedian(int arr[], int n, int brr[], int m){
+    vector<int>v;
+    for(int i=0;i<n;i++)
+        v.emplace_back(arr[i]);
+    for(int i=0;i<m;i++)
+        v.emplace_back(brr[i]);
+    sort(v.begin(),v.end());
+    if(v.size()%2==0)
+        return (v[v.size()/2]+v[v.size()/2-1])/2;
+    return v[v.size()/2];
+}
