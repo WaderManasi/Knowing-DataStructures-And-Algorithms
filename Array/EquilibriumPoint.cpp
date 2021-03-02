afda614e7bf178dc42b0d990bab9c2403d232d4b
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-03-03 00:53:16 +0530

    Equi point

--- Array/EquilibriumPoint.cpp
+++ Array/EquilibriumPoint.cpp
@@ -2,7 +2,7 @@
 // Equilibrium index of an array is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes.
 // Given an array, your task is to find the index of first Equilibrium point in the array.
 
-//
+// Solution:
 int findEquilibrium(int arr[], int n)
 {
   int right=0,left=0;
