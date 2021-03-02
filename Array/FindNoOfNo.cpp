d311827391097f2c13e2ec6dbe80279832f2fce3
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-03-02 13:19:39 +0530

    Add

--- Array/FindNoOfNo.cpp
+++ Array/FindNoOfNo.cpp
@@ -1,4 +1,6 @@
-/*Complete the function given below*/
+// Given an array A[]  of n elements.
+// Your task is to complete the Function num which returns an integer denoting the total number of times digit k appears in the whole array.
+
 int num(int a[], int n, int k)
 {
     int ct=0;
