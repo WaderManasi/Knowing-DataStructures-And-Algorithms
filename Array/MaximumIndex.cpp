045366ef9ca6c881f4bc1a1a2112bb5047028ddb
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-03-03 09:59:38 +0530

    Max Index

--- Array/MaximumIndex.cpp
+++ Array/MaximumIndex.cpp
@@ -1,3 +1,6 @@
+// Given an array A[] of N positive integers.
+// The task is to find the maximum of j - i subjected to the constraint of A[i] <= A[j].
+ 
 int maxIndexDiff(int arr[], int n) 
 { 
     int i=0,j=n-1,m=0,d=0;
@@ -13,6 +16,5 @@ int maxIndexDiff(int arr[], int n)
             else
             j--;
    }
-    return m;
-    
+    return m;  
 }
