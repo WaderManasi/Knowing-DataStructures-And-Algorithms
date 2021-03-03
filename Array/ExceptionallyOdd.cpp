42cf6e0a84a1c0df2e1d636bdce4569a0f193b56
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-03-03 10:28:09 +0530

    odd freq

--- Array/ExceptionallyOdd.cpp
+++ Array/ExceptionallyOdd.cpp
@@ -1,4 +1,4 @@
-Your task is to complete the function getOddOccurrence() which takes arr[] and n as input parameters and returns the exceptional number.
+// Your task is to complete the function getOddOccurrence() which takes arr[] and n as input parameters and returns the exceptional number.
 
 class Solution{   
 public:
